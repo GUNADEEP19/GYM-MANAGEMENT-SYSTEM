0c1f96c55bd866dcf3d8eb834436db711d5b8b47
GUNADEEP19 <gunadeep19082005@gmail.com>
2026-03-29 17:55:40 +0530

    Initial Spring Boot setup

---
+++ gym-management-system-backend/src/main/java/com/gym/backend/GymManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gym.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymManagementSystemBackendApplication.class, args);
+	}
+
+}
